43bc8347e1b716a50bb3e11d2050c470b8eff5a0
Qiox <72603407+Qiox@users.noreply.github.com>
2020-10-10 23:27:00 +0300

    Update main.scala

--- Herogi_CaseStudy/src/main/scala/main.scala
+++ Herogi_CaseStudy/src/main/scala/main.scala
@@ -8,10 +8,10 @@ object TestApp extends App {
   spark.sparkContext.setLogLevel("WARN")
   import spark.implicits._ // Required to call the .toDF function later
   
-  val html = scala.io.Source.fromURL("https://raw.githubusercontent.com/Qiox/Herogi-CaseStudy/main/pace.csv").mkString // Get all rows as one string
-  val html2 = scala.io.Source.fromURL("https://raw.githubusercontent.com/Qiox/Herogi-CaseStudy/main/users.csv").mkString // Get all rows as one string
+  val html_pace = scala.io.Source.fromURL("https://raw.githubusercontent.com/Qiox/Herogi-CaseStudy/main/pace.csv").mkString // Get all rows as one string
+  val html_user = scala.io.Source.fromURL("https://raw.githubusercontent.com/Qiox/Herogi-CaseStudy/main/users.csv").mkString // Get all rows as one string
 
-  val m1 = html.split("\\s") // Split based on white spaces at the end of all rows except the last one
+  val map_pace = html_pace.split("\\s") // Split based on white spaces at the end of all rows except the last one
                  .filter(_ != "") // Filter out any empty lines
                  .filter(! _.contains("userid"))
                  .filter(! _.contains("total_time"))
@@ -20,7 +20,7 @@ object TestApp extends App {
                  .map(row => row.split(",")) // Split each line on ',' to make an Array of 3 String each
                  .map { case Array(f1,f2,f3) => (f1,f2,f3) }// Convert that Array[String] into Array[(String, String, String)]               
  
-  val m2 = html2.split("\\s") // Split based on white spaces at the end of all rows except the last one
+  val map_user = html_user.split("\\s") // Split based on white spaces at the end of all rows except the last one
                  .filter(_ != "") // Filter out any empty lines
                  .filter(! _.contains("userid"))
                  .filter(! _.contains("username"))
@@ -29,19 +29,21 @@ object TestApp extends App {
                  .map(row => row.split(",")) // Split each line on ',' to make an Array of 3 String each
                  .map { case Array(f1,f2,f3) => (f1,f2,f3) }// Convert that Array[String] into Array[(String, String, String)]  
                   
-  val df1 = m1.toDF("userid", "total_time (min)", "distance (m)") //Creating Pace dataframe
-  val df2 = m2.toDF("userid", "username", "age")  //Creating Users dataframe
+  val df_pace = map_pace.toDF("userid", "total_time (min)", "distance (m)") //Creating Pace dataframe
+  val df_user = map_user.toDF("userid", "username", "age")  //Creating Users dataframe
   
   import org.apache.spark.sql.functions._  //For SQL operations
-  
-  val df3 = df2.join(df1, ("userid")).drop("userid")  //Joining Pace and Users dataframe by their mutual userid column
-  val result = df3.withColumn("average_pace (km / min)", format_number((($"distance (m)"/1000).as[Double] / $"total_time (min)".as[Double]),2)).sort(desc("average_pace (km / min)"))  //Calculating the average pace and getting rid of extra decimals
+  val sex = Seq(("user1","--"),("ahmet","male"),("yunus","male"),("john","male"),("adam","male"),("brian","male"),("tyler","male"),("ceren","female"),("hasan","male"),("selen","female"),("diana","female"),("thomas","male"),("james","male"),("janet","female"))
+  val df_sex = sex.toDF("username","sex")  //Creating sex data for every user
+  val df_user_sex = df_user.join(df_sex, ("username"))  //Adding sex data into user dataframe
+  val df_merge = df_user_sex.join(df_pace, ("userid")).drop("userid")  //Joining Pace and Users dataframe by their mutual userid column
+  val df_result = df_merge.withColumn("average_pace (km / min)", format_number((($"distance (m)"/1000).as[Double] / $"total_time (min)".as[Double]),2)).sort(desc("average_pace (km / min)"))  //Calculating the average pace and getting rid of extra decimals
                                                                                                                                                                                        //while creating the coresponding column and add it to the final dataframe
   
-  df1.show()
-  df2.show()
-  df3.show()
-  result.show()
+  df_pace.show()
+  df_user_sex.show()
+  df_merge.show()
+  df_result.show()
 
   spark.close() // don't forget to close(), otherwise scastie won't let you create another session so soon.
 }
